25877d5d1f2a9e650e4cc36114b813d650af34fc
profits <v-profit@yandex.ru>
2021-04-02 18:47:08 +0400

    08-урок выполнен

--- lesson_002/08_garden.py
+++ lesson_002/08_garden.py
@@ -8,20 +8,19 @@ garden = ('ромашка', 'роза', 'одуванчик', 'ромашка', 
 meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер', 'мак', 'одуванчик', 'ромашка', )
 
 # создайте множество цветов, произрастающих в саду и на лугу
-# garden_set =
-# meadow_set =
-# TODO здесь ваш код
+garden_set={garden[0],garden[1],garden[2],garden[3],garden[4],garden[5],garden[6]}
+meadow_set={meadow[0],meadow[1],meadow[2],meadow[3],meadow[4],meadow[5],meadow[6]}
 
 # выведите на консоль все виды цветов
-# TODO здесь ваш код
+print(garden_set|meadow_set)
 
 # выведите на консоль те, которые растут и там и там
-# TODO здесь ваш код
+print(garden_set&meadow_set)
 
 # выведите на консоль те, которые растут в саду, но не растут на лугу
-# TODO здесь ваш код
+print(garden_set-meadow_set)
 
 # выведите на консоль те, которые растут на лугу, но не растут в саду
-# TODO здесь ваш код
+print(meadow_set-garden_set)
 
 
